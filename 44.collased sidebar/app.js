851ce2213b3d5da16db9a2f0b6ce3284900e4097
28vikas <63vikas@gmail.com>
2023-06-07 01:42:22 +0530

    collased sidebar

---
+++ 44.collased sidebar/app.js
@@ -0,0 +1,19 @@
+
+
+let btn = document.getElementById("sidebar");
+let head = document.getElementById("head");
+let closeBtn = document.getElementById("sidebar");
+
+function xyz(){
+btn.style.width = "250px"
+head.style.marginLeft = "250px"
+
+}
+
+
+
+function openNav(){
+closeBtn.style.width = "0px"
+head.style.marginLeft = "0px"
+
+}
