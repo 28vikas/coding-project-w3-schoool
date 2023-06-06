8408bd0a35a08f0767dde509002440a200535d7a
28vikas <63vikas@gmail.com>
2023-06-06 23:49:45 +0530

    collasped sidepanel

---
+++ 45.Collaspsed sidepanel/app.js
@@ -0,0 +1,9 @@
+
+
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
