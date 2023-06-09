0459cf2bd7177a9d48018ee519aad4b699c959a4
28vikas <63vikas@gmail.com>
2023-06-09 17:29:58 +0530

    Mobile menu

---
+++ 42.Mobile Menu/app.js
@@ -0,0 +1,9 @@
+let link = document.getElementById("box");
+
+function showBar() {
+  if (link.style.display === "none") {
+    link.style.display = "block";
+  } else{
+    link.style.display = "none";
+  }
+}
