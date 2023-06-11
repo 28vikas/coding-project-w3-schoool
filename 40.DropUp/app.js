83f4b9965241cb6c25ec31d46675d1eaca8731ab
28vikas <63vikas@gmail.com>
2023-06-11 18:14:56 +0530

    dropup

---
+++ 40.DropUp/app.js
@@ -0,0 +1,15 @@
+
+let dropContent = document.getElementById("drop-content");
+
+
+function xyz(){
+    
+    dropContent.style.height = "fit-content"
+}
+
+
+function abc(){
+        dropContent.style.height ="0" 
+ 
+}
+
