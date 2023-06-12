ed5dc6d6966cfa8dd9bd274241e723f51d8a3f7b
28vikas <63vikas@gmail.com>
2023-06-12 23:39:24 +0530

    Drop down sidenav

---
+++ 37.Drop Down InsideNav/app.js
@@ -0,0 +1,11 @@
+const dropdown = document.getElementById("dropdown");
+
+
+function xyz(){
+ 
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+      } else {
+        dropdown.style.display = "block";
+      }
+}
