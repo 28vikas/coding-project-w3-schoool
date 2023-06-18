5183c594e3b140d931ca5ac1f5ed74b6db62513a
28vikas <63vikas@gmail.com>
2023-06-18 09:58:06 +0530

    side navigation

---
+++ 16 side Navigation/app.js
@@ -0,0 +1,15 @@
+
+let sidenav = document.getElementById("sidenavbar")
+
+
+
+function openNav(){
+    sidenav.style.transform = " translateX(0px)"
+    
+}
+
+
+function closeNav(){
+    sidenav.style.transform = " translateX(-200px)"
+
+}
