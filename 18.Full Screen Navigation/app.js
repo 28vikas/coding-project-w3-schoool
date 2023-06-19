150f7ddb100d986705a298696ffe3ad7fdd61956
28vikas <63vikas@gmail.com>
2023-06-19 09:40:33 +0530

    full screen navigation

---
+++ 18.Full Screen Navigation/app.js
@@ -0,0 +1,15 @@
+
+let sidenav = document.getElementById("sidenavbar")
+
+
+
+function openNav(){
+    sidenav.style.transform = " translateX(0vw)"
+    
+}
+
+
+function closeNav(){
+    sidenav.style.transform = " translateX(-100vw)"
+
+}
